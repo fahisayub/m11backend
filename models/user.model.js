dc5ca1c76318f9f451555b9e9d050e0b8bf53a5d
MUHAMMED FAHIZ P A <fahisayub@gmail.com>
2023-01-19 13:27:45 +0530

    m11backend

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  uid: { type: String },
   email: { type: String, required: true },
   password: { type: String, required: true },
 });
